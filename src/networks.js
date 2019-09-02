4ffb0e917b4953ce378956193ff4d5ebd8e27d95
dezzyboy <dezzyboy@tradebit.ca>
2019-09-02 11:19:48 -0400

    update network

--- src/networks.js
+++ src/networks.js
@@ -2,25 +2,25 @@
 Object.defineProperty(exports, '__esModule', { value: true });
 exports.bitcoin = {
   messagePrefix: '\x18Bitcoin Signed Message:\n',
-  bech32: 'gb',
+  bech32: 'bc',
   bip32: {
     public: 0x0488b21e,
     private: 0x0488ade4,
   },
-  pubKeyHash: 0x4d,
-  scriptHash: 0x8b,
-  wif: 0xd0,
+  pubKeyHash: 0x00,
+  scriptHash: 0x05,
+  wif: 0x80,
 };
 exports.regtest = {
   messagePrefix: '\x18Bitcoin Signed Message:\n',
-  bech32: 'gbrt',
+  bech32: 'bcrt',
   bip32: {
     public: 0x043587cf,
     private: 0x04358394,
   },
-  pubKeyHash: 0x3c,
+  pubKeyHash: 0x6f,
   scriptHash: 0xc4,
-  wif: 0xbc,
+  wif: 0xef,
 };
 exports.testnet = {
   messagePrefix: '\x18Bitcoin Signed Message:\n',
@@ -29,7 +29,7 @@ exports.testnet = {
     public: 0x043587cf,
     private: 0x04358394,
   },
-  pubKeyHash: 0x41,
+  pubKeyHash: 0x6f,
   scriptHash: 0xc4,
-  wif: 0xc1,
+  wif: 0xef,
 };
